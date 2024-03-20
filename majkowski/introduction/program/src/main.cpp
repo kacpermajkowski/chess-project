7b44fc70cc21fa771c70987c4e8e030bd358251a
Kacper Majkowski <251578@edu.p.lodz.pl>
2024-03-20 21:52:44 +0100

    Math helper factorial

--- majkowski/introduction/program/src/main.cpp
+++ majkowski/introduction/program/src/main.cpp
@@ -1,10 +1,12 @@
 // Introduction project.
 
 #include <iostream>
+#include "math_helpers.h"
 using namespace std;
 
 int main()
 {
 	cout << "Hello, world!" << endl;
+	cout << "factorial(5) = " << factorial(5) << endl;
 	return 0;
 }
