4f797f7024627262b93b7a919739bf257338ff6f
Kacper Majkowski <251578@edu.p.lodz.pl>
2024-03-20 22:21:07 +0100

    More factorials

--- majkowski/introduction/program/src/main.cpp
+++ majkowski/introduction/program/src/main.cpp
@@ -7,6 +7,10 @@ using namespace std;
 int main()
 {
 	cout << "Hello, world!" << endl;
+	cout << "factorial(1) = " << factorial(1) << endl;
+	cout << "factorial(2) = " << factorial(2) << endl;
+	cout << "factorial(3) = " << factorial(3) << endl;
+	cout << "factorial(4) = " << factorial(4) << endl;
 	cout << "factorial(5) = " << factorial(5) << endl;
 	return 0;
 }
