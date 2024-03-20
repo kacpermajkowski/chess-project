e26beb17a0cfb2e9e0bb5ca2c04427d8f345b364
Kacper Majkowski <251578@edu.p.lodz.pl>
2024-03-20 22:22:52 +0100

    introduction message

--- majkowski/introduction/program/src/main.cpp
+++ majkowski/introduction/program/src/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+	cout << "Introduction project." << endl << endl;
 	cout << "Hello, world!" << endl;
 	cout << "factorial(5) = " << factorial(5) << endl;
 	return 0;
