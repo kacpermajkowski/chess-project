984e438262bc09a4d333c7283c7b4b16b13ee1fd
251578 Majkowski Kacper <251578@edu.p.lodz.pl>
2024-03-28 00:16:55 +0100

    etap 0

---
+++ majkowski/workshop/library/test/ClientTest.cpp
@@ -0,0 +1,9 @@
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(TestSuiteClient)
+
+    BOOST_AUTO_TEST_CASE(AssertionsTests) {
+        BOOST_TEST(true);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
