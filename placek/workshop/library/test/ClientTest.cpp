47129e61ec9fafdbac18665f96afdf1bb16001bb
Aleksandra Placek <251612@edu.p.lodz.pl>
2024-04-11 11:47:00 +0200

    warsztat2:etap2 (placek)

--- placek/workshop/library/test/ClientTest.cpp
+++ placek/workshop/library/test/ClientTest.cpp
@@ -1,4 +1,5 @@
 #include <boost/test/unit_test.hpp>
+#include "model/Client.h"
 
 BOOST_AUTO_TEST_SUITE(TestSuiteClient)
 
@@ -6,5 +7,22 @@ BOOST_AUTO_TEST_CASE(AssertionsTests) {
         BOOST_TEST(1.0/3.0 == 0.333, boost::test_tools::tolerance(0.01));
         BOOST_TEST(true);
 }
+
+Client client("Adam", "Nowak", "123");
+BOOST_AUTO_TEST_CASE(TestConstructorClient){
+    BOOST_TEST(client.getFirstName()=="Adam");
+    BOOST_TEST(client.getLastName()=="Nowak");
+    BOOST_TEST(client.getPersonalID()=="123");
+}
+
+BOOST_AUTO_TEST_CASE(TestSettersClient){
+    client.setFirstName("Jan");
+    client.setLastName("Kowalski");
+    client.setFirstName("");
+    client.setLastName("");
+    BOOST_TEST(client.getFirstName()=="Jan");
+    BOOST_TEST(client.getLastName()=="Kowalski");
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
