82a4b6b5599dc6fb9bd1cb152289b3d169d987b4
Kacper Majkowski <251578@edu.p.lodz.pl>
2024-04-11 10:52:31 +0200

    Brought back missing introduction directories

---
+++ majkowski/introduction/program/src/main.cpp
@@ -0,0 +1,17 @@
+// Introduction project.
+
+#include <iostream>
+#include "math_helpers.h"
+using namespace std;
+
+int main()
+{
+	cout << "Introduction project." << endl << endl;
+	cout << "Hello, world!" << endl;
+	cout << "factorial(1) = " << factorial(1) << endl;
+	cout << "factorial(2) = " << factorial(2) << endl;
+	cout << "factorial(3) = " << factorial(3) << endl;
+	cout << "factorial(4) = " << factorial(4) << endl;
+	cout << "factorial(5) = " << factorial(5) << endl;
+	return 0;
+}
---
+++ placek/introduction/program/src/main.cpp
@@ -0,0 +1,12 @@
+//introduction project
+
+#include <iostream>
+#include "MATH_HELPERS.H"
+using namespace std;
+
+int main(){
+
+	cout << "hello world" << endl;
+	cout << "factorial(5) = " << factorial(5) << endl;
+	return 0;
+}
