fdaad8bef4006915912601b09e18318a78116e5f
Kacper Majkowski <251578@edu.p.lodz.pl>
2024-04-16 18:48:38 +0200

    feat: now using boost fixtures in TestSuiteClient

--- majkowski/workshop/library/test/ClientTest.cpp
+++ majkowski/workshop/library/test/ClientTest.cpp
@@ -1,7 +1,29 @@
 #include <boost/test/unit_test.hpp>
 #include "../include/model/Client.h"
 
-BOOST_AUTO_TEST_SUITE(TestSuiteClient)
+struct TestSuiteClientFixture{
+    const std::string testFirstName = "Kacper";
+    const std::string testLastName = "Majkowski";
+    const std::string testPersonalID = "251578";
+
+    const std::string newTestFirstName = "Robert";
+    const std::string newTestLastName = "Rządziński";
+
+    Address *testAddress;
+    Address *newTestAddress;
+
+    TestSuiteClientFixture(){
+        testAddress = new Address("Warszawa", "Srebrna", "17");
+        newTestAddress = new Address("Łódź", "al. Politechniki", "22");
+    }
+
+    ~TestSuiteClientFixture(){
+        delete testAddress;
+        delete newTestAddress;
+    }
+};
+
+BOOST_FIXTURE_TEST_SUITE(TestSuiteClient, TestSuiteClientFixture)
 
     ///@brief Example assertion tests
     BOOST_AUTO_TEST_CASE(AssertionsTests) {
@@ -9,24 +31,29 @@ BOOST_AUTO_TEST_SUITE(TestSuiteClient)
         BOOST_TEST(true);
     }
 
-    Client client("Kacper", "Majkowski", "251578");
     ///@brief Checks if Client getters return expected values after setting them via constructor
     BOOST_AUTO_TEST_CASE(ClientConstrutorTests){
-        BOOST_TEST(client.getFirstName() == "Kacper");
-        BOOST_TEST(client.getLastName() == "Majkowski");
-        BOOST_TEST(client.getPerosnalID() == "251578");
+        Client client(testFirstName, testLastName, testPersonalID, testAddress);
+        BOOST_TEST(client.getFirstName() == testFirstName);
+        BOOST_TEST(client.getLastName() == testLastName);
+        BOOST_TEST(client.getPersonalID() == testPersonalID);
+        BOOST_TEST(client.getAddress() == testAddress);
     }
 
     ///@brief Checks if using setters changes value returned by getters
     BOOST_AUTO_TEST_CASE(ClientSettersTests){
-        client.setFirstName("Robert");
-        client.setLastName("Rządziński");
-        BOOST_TEST(client.getFirstName() == "Robert");
-        BOOST_TEST(client.getLastName() == "Rządziński");
+        Client client(testFirstName, testLastName, testPersonalID, testAddress);
+        client.setFirstName(newTestFirstName);
+        client.setLastName(newTestLastName);
+        client.setAddress(newTestAddress);
+        BOOST_TEST(client.getFirstName() == newTestFirstName);
+        BOOST_TEST(client.getLastName() == newTestLastName);
+        BOOST_TEST(client.getAddress() == newTestAddress);
     }
 
     ///@brief Checks if getters still return the same value after using setters with empty string param
     BOOST_AUTO_TEST_CASE(ClientSettersEmptyStringTests){
+        Client client(testFirstName, testLastName, testPersonalID, testAddress);
         std::string firstName = client.getFirstName();
         std::string lastName = client.getLastName();
         client.setFirstName("");
@@ -34,7 +61,5 @@ BOOST_AUTO_TEST_SUITE(TestSuiteClient)
         BOOST_TEST(client.getFirstName() == firstName);
         BOOST_TEST(client.getLastName() == lastName);
     }
-
-
 
 BOOST_AUTO_TEST_SUITE_END()
