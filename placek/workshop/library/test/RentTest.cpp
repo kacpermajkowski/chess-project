3cf19746c233ae15c4f272e51e5bf8f520528455
Aleksandra Placek <251612@edu.p.lodz.pl>
2024-05-09 11:46:43 +0200

    warsztat3:etap3 (placek)

--- placek/workshop/library/test/RentTest.cpp
+++ placek/workshop/library/test/RentTest.cpp
@@ -3,6 +3,33 @@
 #include "model/Client.h"
 #include "model/Vehicle.h"
 
-BOOST_AUTO_TEST_SUITE(TestSuiteRent)
+struct TestSuiteRentFixture{
+    Client* client;
+    Address* address;
+    Vehicle* vehicle;
+    unsigned int rentID;
+
+    TestSuiteRentFixture(){
+        address = new Address("Poznan", "Ogrodowa", "15");
+        client = new Client("Adam", "Nowak", "123", address);
+        vehicle = new Vehicle("GD12345", 600);
+        rentID = 1;
+    }
+    ~TestSuiteRentFixture(){
+        delete address;
+        delete client;
+        delete vehicle;
+    }
+
+};
+
+BOOST_FIXTURE_TEST_SUITE(TestSuiteRent, TestSuiteRentFixture)
+
+    BOOST_AUTO_TEST_CASE(TestConstructorRent){
+    Rent rent(rentID, client, vehicle);
+        BOOST_TEST(rent.getId()==rentID);
+        BOOST_TEST(rent.getClient()==client);
+        BOOST_TEST(rent.getVehicle()==vehicle);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
