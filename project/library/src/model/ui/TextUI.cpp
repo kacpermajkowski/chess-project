5706ee40e21fb4f08765e07b694203bef2899f64
Kacper Majkowski <251578@edu.p.lodz.pl>
2024-06-16 00:12:43 +0200

    feat: better text user interface TextUI

--- project/library/src/model/ui/TextUI.cpp
+++ project/library/src/model/ui/TextUI.cpp
@@ -9,12 +9,31 @@
 #include "model/UnitDir/Knight.h"
 #include "model/UnitDir/Pawn.h"
 #include "model/State.h"
-
+#include "../src/model/util/util.cpp"
 
 #include <locale>
 #include <codecvt>
 #include <string>
 #include <map>
+
+wchar_t* concatenate(const wchar_t* str1, const wchar_t* str2) {
+    // Calculate the length of the concatenated string
+    size_t len1 = wcslen(str1);
+    size_t len2 = wcslen(str2);
+    size_t totalLen = len1 + len2;
+
+    // Allocate memory for the new string
+    // +1 for the null terminator
+    wchar_t* result = new wchar_t[totalLen + 1];
+
+    // Copy the first string to the result
+    wcscpy(result, str1);
+
+    // Concatenate the second string to the result
+    wcscat(result, str2);
+
+    return result;
+}
 
 void TextUI::update(StatePtr state) {
     std::locale::global(std::locale("en_US.UTF-8")); //nw czy w dobrym miejscu
@@ -36,40 +55,85 @@ void TextUI::update(StatePtr state) {
     wchar_t blackKnight = L'♞';
     wchar_t blackPawn = L'♟';
 
+    // ANSI escape codes for colors
+    const wchar_t * RESET = L"\033[0m";
+    const wchar_t * BLACK_BG = L"\033[44m";
+    const wchar_t * WHITE_BG = L"\033[47m";
+
+    const wchar_t * WHITE_FG = L"\033[97m";
+    const wchar_t * BLACK_FG = L"\033[30m";
+
     std::wcout << std::endl;
-    wchar_t unitSymbol = L' ';
-    for (int rows = 7; rows >= 0; rows--) {
-        for (int line = 0; line <= 2; line++) {
-            for (int column = 0; column <= 7; column++) {
-
-                wchar_t block = ((rows + column) % 2 == 0) ? blackSquare : whiteSquare;
-
-                if (line == 1) {
-                    FieldPtr f = state->getBoard()->getField(std::make_shared<Position>(LetterIndex(column), NumberIndex(rows)));
-                    if (f->getUnit() != nullptr) {
-                        UnitPtr currentUnit = f->getUnit();
-                        unitSymbol = blackBishop;
-                        if (typeid(*currentUnit) == typeid(King))
-                            unitSymbol = currentUnit->getColor() == WHITE ? blackKing : whiteKing;
-                        else if (typeid(*currentUnit) == typeid(Queen))
-                            unitSymbol = currentUnit->getColor() == WHITE ? blackQueen : whiteQueen;
-                        else if (typeid(*currentUnit) == typeid(Rook))
-                            unitSymbol = currentUnit->getColor() == WHITE ? blackRook : whiteRook;
-                        else if (typeid(*currentUnit) == typeid(Bishop))
-                            unitSymbol = currentUnit->getColor() == WHITE ? blackBishop : whiteBishop;
-                        else if (typeid(*currentUnit) == typeid(Knight))
-                            unitSymbol = currentUnit->getColor() == WHITE ? blackKnight : whiteKnight;
-                        else if (typeid(*currentUnit) == typeid(Pawn))
-                            unitSymbol = currentUnit->getColor() == WHITE ? blackPawn : whitePawn;
-                    } else unitSymbol = block;
-                    std::wcout << block << L' ' << unitSymbol << L' ' << block << L' ';
-                } else {
-                    std::wcout << block << L' ' << block << L' ' << block << L' ';
-                }
+    for(int rows = 7; rows >= 0; rows--){
+        std::wcout << (wchar_t)(L'1'+rows) << L" ";
+        for(int column = 7; column >= 0; column--){
+            if((rows + column) % 2 == 0){
+                std::wcout << BLACK_BG;
+            } else{
+                std::wcout << WHITE_BG;
             }
-            std::wcout << std::endl;
+
+            std::wcout << L"  ";
+            FieldPtr f = state->getBoard()->getField(std::make_shared<Position>(LetterIndex(column), NumberIndex(rows)));
+            if (f->getUnit() != nullptr) {
+                UnitPtr currentUnit = f->getUnit();
+                //Font color
+                std::wcout << ((currentUnit->getColor() == BLACK) ? BLACK_FG : WHITE_FG);
+                //Icon
+                if (isTypeOf<King>(currentUnit))
+                    std::wcout << ((currentUnit->getColor() == BLACK) ? blackKing : whiteKing);
+                else if (isTypeOf<Queen>(currentUnit))
+                    std::wcout << ((currentUnit->getColor() == BLACK) ? blackQueen : whiteQueen);
+                else if (isTypeOf<Rook>(currentUnit))
+                    std::wcout << ((currentUnit->getColor() == BLACK) ? blackRook : whiteRook);
+                else if (isTypeOf<Bishop>(currentUnit))
+                    std::wcout << ((currentUnit->getColor() == BLACK) ? blackBishop : whiteBishop);
+                else if (isTypeOf<Knight>(currentUnit))
+                    std::wcout << ((currentUnit->getColor() == BLACK) ? blackKnight : whiteKnight);
+                else if (isTypeOf<Pawn>(currentUnit))
+                    std::wcout << ((currentUnit->getColor() == BLACK) ? blackPawn : whitePawn);
+            } else std::wcout << L"   ";
+
+            std::wcout << L"  " << RESET;
         }
+        std::wcout << std::endl;
     }
+    std::wcout << L"  " << L"    A      B      C      D      E      F      G      H"<< std::endl;
+
+//    std::wcout << std::endl;
+//    wchar_t unitSymbol = L' ';
+//    for (int rows = 7; rows >= 0; rows--) {
+//        for (int line = 0; line <= 2; line++) {
+//            for (int column = 0; column <= 7; column++) {
+//
+//                wchar_t block = ((rows + column) % 2 == 0) ? blackSquare : whiteSquare;
+//
+//                if (line == 1) {
+//                    FieldPtr f = state->getBoard()->getField(std::make_shared<Position>(LetterIndex(column), NumberIndex(rows)));
+//                    if (f->getUnit() != nullptr) {
+//                        UnitPtr currentUnit = f->getUnit();
+//                        unitSymbol = blackBishop;
+//                        if (typeid(*currentUnit) == typeid(King))
+//                            unitSymbol = currentUnit->getColor() == WHITE ? blackKing : whiteKing;
+//                        else if (typeid(*currentUnit) == typeid(Queen))
+//                            unitSymbol = currentUnit->getColor() == WHITE ? blackQueen : whiteQueen;
+//                        else if (typeid(*currentUnit) == typeid(Rook))
+//                            unitSymbol = currentUnit->getColor() == WHITE ? blackRook : whiteRook;
+//                        else if (typeid(*currentUnit) == typeid(Bishop))
+//                            unitSymbol = currentUnit->getColor() == WHITE ? blackBishop : whiteBishop;
+//                        else if (typeid(*currentUnit) == typeid(Knight))
+//                            unitSymbol = currentUnit->getColor() == WHITE ? blackKnight : whiteKnight;
+//                        else if (typeid(*currentUnit) == typeid(Pawn))
+//                            unitSymbol = currentUnit->getColor() == WHITE ? blackPawn : whitePawn;
+//                    } else unitSymbol = block;
+//                    std::wcout << block << L' ' << unitSymbol << L' ' << block << L' ';
+//                } else {
+//                    std::wcout << block << L' ' << block << L' ' << block << L' ';
+//                }
+//            }
+//            std::wcout << std::endl;
+//        }
+//    }
 }
 
 void TextUI::endGameScreen(StatePtr state) {
